91d4e1a8f5ad939f624c0306ab65793f7e208720
mandros1 <marinandros031@gmail.com>
2019-01-11 09:50:06 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FileUploader} from 'ng2-file-upload';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trial-app';
+  uploadUrl = 'https://jsonplaceholder.typicode.com/posts';  // URL to web api
+  uploader = new FileUploader({url: this.uploadUrl});
+  postList: any = [];
+  csvData;
+
+  fileToUpload: File = null;
+  headers = new HttpHeaders({
+    'Content-Type': 'application/vnd.ms-excel',
+    'Accept': 'application/json'
+  });
+
+  constructor(private http: HttpClient) {
+    this.uploader.onCompleteItem = (item , response, status, headers) => {
+      this.postList.push(JSON.parse(response));
+    };
+  }
+
+  uploadFile(fileAsStream) {
+    const url = this.uploadUrl;
+    console.log(`Upload => ${fileAsStream}`);
+    console.log(this.csvData);
+    return this.http.post(url, this.csvData, {headers: this.headers, responseType: 'blob' as 'json'});
+  }
+
+  handleFileInput(files: FileList) {
+    this.changeListener(files);
+    this.fileToUpload = files.item(0);
+    this.uploadFile(this.fileToUpload).subscribe(
+        data => {
+          console.log(`Success -> ${this.fileToUpload.name} and ${this.fileToUpload.type} = ${this.fileToUpload.slice()}`);
+        },
+        err => {
+          console.log('Error');
+        }
+    );
+  }
+
+  public changeListener(files: FileList) {
+    console.log(files);
+    if (files && files.length > 0) {
+      const file: File = files.item(0);
+      const reader: FileReader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = (e) => {
+        this.csvData = reader.result;
+      };
+    }
+  }
 }
